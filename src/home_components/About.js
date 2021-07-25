c92164f8b0b0734117ff99121319338d79715abf
Sid <sid.8491@gmail.com>
2021-07-25 11:30:27 +0530

    changed logo and about us

--- src/home_components/About.js
+++ src/home_components/About.js
@@ -15,44 +15,25 @@ function About() {
         <div className="container block-padding pt-0">
           <div className="row">
             <div className="col-lg-6">
-              <h3>Some Heading</h3>
-              <p><strong>NAME OF OWNER</strong></p>
+              <h3>Dr. Harinder Yant & Mr. Sonu Khatri</h3>
+              {/* <p><strong>NAME OF OWNER</strong></p> */}
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                volutpat venenatis metus. Duis urna sem, efficitur sit amet
-                neque ac, suscipit accumsan libero. Ut in ultricies lorem. Nam
-                ac ipsum velit. Aenean id nunc lectus. Sed hendrerit viverra
-                arcu, eget congue est pellentesque vitae. Vestibulum eu orci
-                ante. Nulla neque tellus, imperdiet vitae finibus vitae, egestas
-                id dui.
+                Dr. Harinder Yant is a radiologist (MBBS + MD) and Mr. Sonu
+                Khatri is a sports personality. Both of them were very fond of
+                dogs from the childhood. Opening this venture was their
+                childhood dream and now that they have enough money from the
+                savings & earnings, they have converted their love and care
+                towards the dog into a profession, hence "Working Dog Club" is
+                founded.
               </p>
-              {/* <p>
-                Sicherheit K9 produce highly trained family protection dogs that
-                are sociable and balanced and yet more than capable of
-                protecting YOU and YOUR FAMILY against any threat or multiple
-                threats. Our dogs are taught to fight through attacks from
-                weapons such as batons, knives and guns.
-              </p> */}
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                volutpat venenatis metus. Duis urna sem, efficitur sit amet
-              </p>
-              {/* <p>
-                People are now only starting to see the great benefit of owning
-                a personal or family protection dog. The serious threat of crime
-                is at our doorstep. Not only will a fully trained dog protect
-                your family and premises/ business but would willingly give
-                their life to protect yours.
-              </p> */}
-              <h6>Bullet points:</h6>
+              {/* <h6>Bullet points:</h6>
               <ul className="custom pl-0">
                 <li>Lorem ipsum dolor</li>
                 <li>consectetur adipiscing elit</li>
                 <li>Nulla neque tellus</li>
                 <li>Imac ipsum velit. Aenean id nunc lectus.</li>
                 <li>Aenean id nunc lectus</li>
-              </ul>
-              {/* <!-- /ul  --> */}
+              </ul> */}
             </div>
             {/* <!-- image  --> */}
             <div className="col-lg-6">
@@ -130,7 +111,7 @@ function About() {
           {/* <!-- /container  --> */}
         </div>
         {/* <!-- /block-padding  --> */}
-        
+
         {/* <!-- /container  --> */}
       </section>
       {/* <!-- / Section Ends  --> */}
